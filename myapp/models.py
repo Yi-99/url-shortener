92e6a187d595b8ddf8a61735f80e08878ce7b37a
Yi-99 <ylim.8299@gmail.com>
2025-05-05 00:53:03 -0700

    initial commit

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Url(models.Model):
+  origin_url = models.TextField()
+  short_url = models.CharField(max_length=255)
+  
+  def __str__(self):
+    return self.origin_url
