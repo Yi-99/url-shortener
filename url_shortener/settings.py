dd551bcb27627644edfc63d8cc5c01121bbe5609
Yi-99 <ylim.8299@gmail.com>
2025-05-05 20:20:34 -0700

    feat: added requirements.txt for build

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-72270%pno8urp63^+jwhi1ci4_76dpoe^k(s(s#qe@a9$-5)#^'
+SECRET_KEY = os.getenvv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('HOST_IP')]
 
 
 # Application definition
